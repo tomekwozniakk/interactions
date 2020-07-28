7824b3562e4f61a2ab6311a3b066a4e227ae6364
Tomek Wozniak <tomekwozniakk@gmail.com>
2020-07-28 20:34:41 +0200

    added functionality - writing names of medicines instead of RXCUI numbers

--- src/js/index.js
+++ src/js/index.js
@@ -6,4 +6,41 @@ import '../scss/main.scss';
 
 /* place your code below */
 
-console.log('HELLO 🚀')
+console.log('HELLO 🚀');
+
+const button = document.querySelector('.button');
+const inputOne = document.querySelector('.drug__name-first');
+const inputTwo = document.querySelector('.drug__name-second');
+const disclaimer = document.querySelector('.disclaimer');
+const comment = document.querySelector('.comment');
+
+
+button.addEventListener('click', (e) =>{
+    const dataOne = inputOne.value;
+    const dataTwo = inputTwo.value;
+    fetch(`https://rxnav.nlm.nih.gov/REST/approximateTerm.json?term=${dataOne}&maxEntries=1&option=1`)
+    .then(resp => resp.json())
+    .then(resp => {
+        let resultOne = resp.approximateGroup.candidate[0].rxcui;
+    fetch(`https://rxnav.nlm.nih.gov/REST/approximateTerm.json?term=${dataTwo}&maxEntries=1&option=1`)
+    .then(resp => resp.json())
+    .then(resp => {
+        let resultTwo = resp.approximateGroup.candidate[0].rxcui;
+    
+    fetch(`https://rxnav.nlm.nih.gov/REST/interaction/list.json?rxcuis=${resultOne}+${resultTwo}&sources=DrugBank`)
+    .then(resp => resp.json())
+    .then(resp => {
+        let disclaimerText = resp.nlmDisclaimer;
+        disclaimer.innerHTML = disclaimerText;
+        if(resp.fullInteractionTypeGroup) {
+        let commentText = resp.fullInteractionTypeGroup[0].fullInteractionType[0].interactionPair[0].description;
+        comment.innerHTML = commentText;
+        console.log(commentText);
+        }
+        else {
+            comment.innerHTML = `There was no interacions to display.` 
+        }
+})})})
+    
+    .catch()
+})
